780ef992114312cf060ea212add585c2c452478b
heinhtet naing <6927125-hhn186@users.noreply.gitlab.com>
2022-09-01 13:55:49 +0630

    refactored product details

--- src/pages/productDetails/ProductDetails.tsx
+++ src/pages/productDetails/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import {  useNavigate, useParams } from "react-router-dom";
 import PropagateLoader from "react-spinners/PropagateLoader";
 
@@ -9,7 +9,6 @@ import {
 	shoeType,
 	wishlistContextType,
 } from "../../../custom";
-
 import {
 	Card
 } from "../../components";
@@ -20,11 +19,15 @@ import {
 } from "../../useContext";
 import { notify } from "../../util";
 
+interface shoeDataType extends shoeType {
+	relatedProducts : Array<RecordType>
+}
+
 const ProductDetails = () => {
 	const navigate = useNavigate();
 	const params = useParams();
 
-	const { cartItems , addToCart, checkIsInCart } = useContext(
+	const {  addToCart, checkIsInCart } = useContext(
 		CartContext
 	) as cartContextType;
 	const { addToWishlist, isInWishlist, removeFromWishlist } = useContext(
@@ -34,7 +37,7 @@ const ProductDetails = () => {
 		ShoeDataContext
 	) as ShoeDataContextType;
 
-	const [shoe, setShoe] = useState<shoeType>({
+	const [shoe, setShoe] = useState<shoeDataType>({
 		name: "",
 		description: "",
 		price: 0,
@@ -43,14 +46,10 @@ const ProductDetails = () => {
 		colour: "",
 		type: "",
 		videoUrl: "",
+		relatedProducts: [ {createdTime : "" , id : "1" ,  fields :[]}],
 	});
-	const [images, setImages] = useState<Array<string>>([]);
-	const [relatedProducts, setRelatedProducts] = useState<Array<RecordType>>([]);
-	const [displayImage, setDisplayImage] = useState<string>("");
-	const [quantity, setQuantity] = useState<number>(1);
-	const [selectedShoeSize, setShoeSize] = useState<number>(38);
-	const [isLiked, setIsLiked] = useState<boolean>(false);
-	const [isInCart, setIsInCart] = useState<boolean>(false);
+	const [gallery , setGallery] = useState({displayImage : '' , otherImages : [] })
+	const [userChoices , setUserChoices] = useState({quantity : 1 , selectedShoeSize : 38 , isLiked :false , isInCart : false})
 
 	const shoeSizes = [38, 39, 40, 41, 42, 43, 44, 45];
 
@@ -63,18 +62,18 @@ const ProductDetails = () => {
 		const imageData = await getImages(shoeData.id);
 		const relatedProductData = await getRandomShoes(5);
 
-		setIsLiked(isInWishlist(shoeData.name));
-		setIsInCart(checkIsInCart(shoeData.name));
 
-		setShoe(shoeData);
-		setImages(
-			imageData[0].fields.images
+		setUserChoices({...userChoices , isLiked : isInWishlist(shoeData.name) ,  isInCart : checkIsInCart(shoeData.name)})
+
+		setShoe({...shoeData , relatedProducts : relatedProductData});
+
+		setGallery({
+			displayImage: shoeData.imageUrl,
+			otherImages: imageData[0].fields.images
 				.split("|")
-				.filter((image: string) => image !== "")
-		);
+				.filter((image: string) => image !== ""),
+		});
 
-		setDisplayImage(shoeData.imageUrl);
-		setRelatedProducts(relatedProductData);
 		window.scrollTo(0, 0);
 	};
 
@@ -82,19 +81,19 @@ const ProductDetails = () => {
 	const handleAddToCart = () => {
 		notify("Added to Cart", "🛒");
 
-		setIsInCart(true)
+		setUserChoices({...userChoices , isInCart: true})
 
 		addToCart({
 			name: shoe.name,
 			price: shoe.price,
-			quantity,
-			shoeSize: selectedShoeSize,
+			quantity : userChoices.quantity,
+			shoeSize: userChoices.selectedShoeSize,
 			image: shoe.imageUrl,
 		});
 	};
 
 	const toggleWishlist = () => {
-		if (!isLiked) {
+		if (!userChoices.isLiked) {
 			addToWishlist({
 				name: shoe.name,
 				price: shoe.price,
@@ -106,7 +105,7 @@ const ProductDetails = () => {
 			notify("Removed from Wishlist", "❤️");
 		}
 
-		setIsLiked(!isLiked);
+			setUserChoices({ ...userChoices, isLiked : !userChoices.isLiked });
 	};
 
 	return (
@@ -116,12 +115,12 @@ const ProductDetails = () => {
 					<div className='flex flex-col justify-center items-center gap-16 lg:gap-20 pb-16 lg:pb-32 lg:flex-row'>
 						<div className='flex flex-col-reverse items-center gap-2 lg:gap-3 lg:flex-row'>
 							<div className='flex justify-center gap-2 lg:justify-start lg:flex-col lg:gap-3'>
-								{images.map((image, index) => {
+								{gallery.otherImages.map((image, index) => {
 									return (
 										<img
 											className='w-10 h-10 lg:w-12 lg:h-12 brightness-50 hover:brightness-100 cursor-pointer'
-											onMouseEnter={() => setDisplayImage(image)}
-											onMouseOut={() => setDisplayImage(shoe.imageUrl)}
+											onMouseEnter={() => setGallery({...gallery , displayImage : image})}
+											onMouseOut={() => setGallery({...gallery , displayImage : shoe.imageUrl})}
 											src={image}
 											key={index}
 											alt='404'
@@ -133,7 +132,7 @@ const ProductDetails = () => {
 							<div className='w-56 h-56 md:w-64 md:h-64 lg:w-96 lg:h-96'>
 								<img
 									className='w-full h-full object-cover'
-									src={displayImage}
+									src={gallery.displayImage}
 									alt='404'
 								/>
 							</div>
@@ -147,22 +146,32 @@ const ProductDetails = () => {
 							<p className='font-medium text-2xl my-4'>${shoe.price}</p>
 
 							<p className='font-medium text-sm'>
-								Shoe Size : {selectedShoeSize}
+								Shoe Size : {userChoices.selectedShoeSize}
 							</p>
 							<ul className='flex gap-2 lg:gap-3 flex-wrap'>
 								{shoeSizes.map((shoeSize, index) => {
-									return selectedShoeSize === shoeSize ? (
+									return userChoices.selectedShoeSize === shoeSize ? (
 										<li
 											className='bg-offwhite p-1 lg:text-xl text-darkgrey font-medium rounded-md lg:p-2 cursor-pointer'
 											key={index}
-											onClick={() => setShoeSize(shoeSize)}>
+											onClick={() =>
+												setUserChoices({
+													...userChoices,
+													selectedShoeSize: shoeSize,
+												})
+											}>
 											{shoeSize}
 										</li>
 									) : (
 										<li
 											className='bg-offwhite  p-1 lg:text-xl text-darkgrey font-medium rounded-md lg:p-2 cursor-pointer brightness-50'
 											key={index}
-											onClick={() => setShoeSize(shoeSize)}>
+											onClick={() =>
+												setUserChoices({
+													...userChoices,
+													selectedShoeSize: shoeSize,
+												})
+											}>
 											{shoeSize}
 										</li>
 									);
@@ -170,7 +179,7 @@ const ProductDetails = () => {
 							</ul>
 
 							<div className='flex gap-5 items-center my-4'>
-								{isInCart ? (
+								{userChoices.isInCart ? (
 									<button
 										className='px-4 py-2 font-medium text-center text-darkgrey bg-gold rounded-sm hover:-translate-x-[2px] hover:-translate-y-[2px] hover:shadow-md transition-transform'
 										onClick={() => navigate("/cart")}>
@@ -184,7 +193,12 @@ const ProductDetails = () => {
 													xmlns='http://www.w3.org/2000/svg'
 													viewBox='0 0 20 20'
 													className='hoverBackgroundEffect rounded-md h-5 w-5'
-													onClick={() => setQuantity(quantity - 1)}
+													onClick={() =>
+														setUserChoices({
+															...userChoices,
+															quantity: userChoices.quantity - 1,
+														})
+													}
 													fill='currentColor'>
 													<path
 														fillRule='evenodd'
@@ -194,14 +208,19 @@ const ProductDetails = () => {
 												</svg>
 											</button>
 											<div className='font-medium mx-2 lg:text-2xl lg:mx-5 w-5 text-center'>
-												{quantity}
+												{userChoices.quantity}
 											</div>
 											<button>
 												<svg
 													xmlns='http://www.w3.org/2000/svg'
 													viewBox='0 0 20 20'
 													className='hoverBackgroundEffect rounded-md h-5 w-5'
-													onClick={() => setQuantity(quantity + 1)}
+													onClick={() =>
+														setUserChoices({
+															...userChoices,
+															quantity: userChoices.quantity + 1,
+														})
+													}
 													fill='currentColor'>
 													<path
 														fillRule='evenodd'
@@ -247,9 +266,9 @@ const ProductDetails = () => {
 									<svg
 										xmlns='http://www.w3.org/2000/svg'
 										className='h-5 w-5'
-										fill={isLiked ? "#f43f5e" : "transparent"}
+										fill={userChoices.isLiked ? "#f43f5e" : "transparent"}
 										viewBox='0 0 24 24'
-										stroke={isLiked ? "#f43f5e" : "white"}
+										stroke={userChoices.isLiked ? "#f43f5e" : "white"}
 										strokeWidth='2'>
 										<path
 											strokeLinecap='round'
@@ -259,7 +278,9 @@ const ProductDetails = () => {
 									</svg>
 								</div>
 								<p className='font-light text-sm'>
-									{isLiked ? "Already in WishList" : "Add to Wishlist"}
+									{userChoices.isLiked
+										? "Already in WishList"
+										: "Add to Wishlist"}
 								</p>
 							</div>
 						</div>
@@ -269,14 +290,14 @@ const ProductDetails = () => {
 						<h2 className='font-bold text-xl'>Related Products</h2>
 
 						<div className='flex flex-wrap justify-center gap-10 py-10 lg:justify-center lg:gap-20  lg:p-0 lg:py-20'>
-							{relatedProducts.map((record) => (
+							{shoe.relatedProducts.map((record) => (
 								<Card shoe={record.fields} key={record.id} id={record.id} />
 							))}
 						</div>
 					</div>
 				</>
 			) : (
-							<div className='container text-center mx-auto my-24	'>
+				<div className='container text-center mx-auto my-24	'>
 					<PropagateLoader color='#ffffff' size={8} />
 				</div>
 			)}
